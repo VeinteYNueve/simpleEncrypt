3eee87f7aa207fe1571d1fb771fe8086d444ae9e
VeinteYNueve <melifs2@illinois.edu>
2025-04-22 21:44:13 -0500

    Added error handling, gitignore, input file for testing.

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,24 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
-        println!("Expected 4 arguments, but got {}. Usage: {} <input file> <output file> <key>", args.len(), args[0]);
+        println!("Error: Expected 4 arguments, but got {}. Usage: {} <input file> <output file> <key>", args.len(), args[0]);
         return;
     }
 
-    let file = fs::read(&args[1]).expect("Unable to read file.");
+    let file = fs::read(&args[1]).expect("Error: Unable to read file.");
+
     if file.len() == 0 {
-        println!("File is empty.");
+        println!("Error: File is empty.");
         return;
     }
+    
+    println!("File size: {} bytes", file.len());
 
     let key = args[3].as_bytes();
+    if key.len() == 0 {
+        println!("Error: Key is empty.");
+        return;
+    }
 
     let mut result = Vec::new();
 
@@ -23,7 +30,7 @@ fn main() {
         result.push(byte);
     }
 
-    fs::write(&args[2], &result).expect("Unable to write to file.");
+    fs::write(&args[2], &result).expect("Error: Unable to write to file.");
 
     println!("Done!! Output file: {}", args[2]); 
 }
